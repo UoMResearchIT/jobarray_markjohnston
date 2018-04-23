05b08f32fd3853d01f6fa571e3262d98b3550592
Gerard Capes <gerard.capes@manchester.ac.uk>
2018-04-23 13:50:02 +0100

    Declare variables before loop

--- mwork2.m
+++ mwork2.m
@@ -1,15 +1,16 @@
 path = ['/home/mbexegc2/Dropbox (The University of Manchester)/application_support/matlab/code_enquiries/mark_johnston/'];      %set the path of videos
 number_vids=1;      %set the number of videos
 
-for ndex = 1:number_vids
-    nu_frame=300;         %set the number of frames for each video
-    framerate=30;         %set the frame rate (FPS) 
+nu_frame=300;         %set the number of frames for each video
+framerate=30;         %set the frame rate (FPS)
+material='data';       %set the name of material
+
+for ndex = 1:number_vids    
     if ndex == 1
         framerate_name=['HeLaM Lysobrite 1mL 4hrs after staining5_t1'];
     else
         framerate_name=['HeLaM Dextran 0.5mgml overnight' num2str(ndex-1) '_t1'];
     end
-    material='data';       %set the name of material
-
+   
 	MVR2 (path,number_vids,nu_frame,framerate,material,framerate_name);
 end
