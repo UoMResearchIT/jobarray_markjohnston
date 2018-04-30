d497cb4a87746fdf65266ac5057e5f1cc820ef8b
Gerard Capes <gerard.capes@manchester.ac.uk>
2018-04-30 09:06:49 +0100

    Remove unnecessary brackets

--- mwork2.m
+++ mwork2.m
@@ -1,4 +1,4 @@
-path = ['/home/mbexegc2/Dropbox (The University of Manchester)/application_support/matlab/code_enquiries/mark_johnston/'];      %set the path of videos
+path = '/home/mbexegc2/Dropbox (The University of Manchester)/application_support/matlab/code_enquiries/mark_johnston/';      %set the path of videos
 videos = dir([path, '*.avi']);
 number_vids = length(videos);      %set the number of videos
 
@@ -7,7 +7,7 @@ material='data';       %set the name of material
 
 for ndex = 1:number_vids    
     if ndex == 1
-        framerate_name=['HeLaM Lysobrite 1mL 4hrs after staining5_t1'];
+        framerate_name='HeLaM Lysobrite 1mL 4hrs after staining5_t1';
     else
         framerate_name=['HeLaM Dextran 0.5mgml overnight' num2str(ndex-1) '_t1'];
     end
